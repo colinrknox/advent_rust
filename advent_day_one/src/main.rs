9658b23610809f055feef0683a5bf2b4d05dffa2
Colin Knox <colin.r.knox@gmail.com>
2023-07-01 22:18:02 -0500

    input file and parser in progress

--- advent_day_one/src/main.rs
+++ advent_day_one/src/main.rs
@@ -1,3 +1,23 @@
+
 fn main() {
-    println!("Hello, world!");
+    let file_contents: String = std::fs::read_to_string(std::path::Path::new("input/elves.txt")).expect("should have been able to read the input file");
+
+    print!("{}", file_contents);
+
+    let parsed_contents: Vec<Vec<&str>> = parse_input(&file_contents); 
+}
+
+fn parse_input(input: &String) -> Vec<Vec<&str>> {
+    let mut result: Vec<Vec<&str>> = vec!();
+    let mut set: Vec<&str> = vec![];
+    for line in input.lines() {
+        print!("{}", line);
+        if line == "" {
+            print!("line is equal to newline");
+            result.push(set.clone());
+            set = vec!();
+        }
+        set.push(line);
+    }
+    return result
 }
