27bb1c2c1cfcc40e16f884a3a6d0c28f2fc22a45
Colin Knox <colin.r.knox@gmail.com>
2023-07-04 22:51:06 -0500

    finished day 2 part 1

--- advent_day_two/src/main.rs
+++ advent_day_two/src/main.rs
@@ -1,8 +1,12 @@
 use std::{collections::HashMap, io::BufRead};
 
 fn main() {
-    println!("Hello, world!");
-    let file = std::fs::read_to_string("input/small_test.txt").expect("Failed to read input file");
+    let file = std::fs::read_to_string("input/test.txt").expect("Failed to read input file");
+
+    let mut move_to_move: HashMap<char, char> = HashMap::new();
+    move_to_move.insert('A', 'X');
+    move_to_move.insert('B', 'Y');
+    move_to_move.insert('C', 'Z');
 
     let mut move_to_point: HashMap<char, usize> = HashMap::new();
     move_to_point.insert('X', 1);
@@ -10,16 +14,18 @@ fn main() {
     move_to_point.insert('Z', 3);
 
     let mut beats: HashMap<char, char> = HashMap::new();
-    beats.insert('A', 'Z');
-    beats.insert('B', 'X');
-    beats.insert('C', 'Y');
+    beats.insert('X', 'Z');
+    beats.insert('Y', 'X');
+    beats.insert('Z', 'Y');
 
     let mut sum: usize = 0;
     for line in file.lines() {
-        let opp = line.chars().nth(0).unwrap();
+        if line == "" {
+            continue;
+        }
+        let opp = *move_to_move.get(&line.chars().nth(0).unwrap()).unwrap();
         let me = line.chars().nth(2).unwrap();
 
-        println!("opp: {}, me: {}", opp, me);
         if opp == me {
             sum += 3;
         } else if *beats.get(&opp).unwrap() != me {
