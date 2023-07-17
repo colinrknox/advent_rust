e8ed6e73a3f728ae757fb0ae1165dbbab279df94
Colin Knox <colin.r.knox@gmail.com>
2023-07-16 19:30:42 -0500

    finished part 1 day 5

--- day_five/src/main.rs
+++ day_five/src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 struct Command(usize, usize, usize);
 
 fn main() {
@@ -5,8 +6,30 @@ fn main() {
     let move_file = std::fs::read_to_string("input/test.txt").unwrap();
 
     let mut stacks: Vec<Vec<char>> = parse_state(&state_file);
+    let commands = parse_commands(&move_file);
 
-    println!("{:?}", stacks);
+    run_commands(&mut stacks, &commands);
+
+    println!(
+        "{}",
+        stacks
+            .iter()
+            .map(|v| v.last())
+            .fold("".to_string(), |p, n| [p, n.unwrap().to_string()].join(""))
+    );
+}
+
+fn run_commands(stacks: &mut Vec<Vec<char>>, commands: &Vec<Command>) {
+    for Command(a, b, c) in commands.iter() {
+        for _ in 0..*a {
+            let val = {
+                let src = stacks.get_mut(b - 1).unwrap();
+                src.pop().unwrap()
+            };
+            let dest = stacks.get_mut(c - 1).unwrap();
+            dest.push(val);
+        }
+    }
 }
 
 fn compute_size(line: &str) -> usize {
@@ -22,6 +45,24 @@ fn create_vec(input: &String) -> Vec<Vec<char>> {
     let mut res: Vec<Vec<char>> = Vec::new();
     for _ in 0..size {
         res.push(Vec::new());
+    }
+    res
+}
+
+fn parse_commands(input: &String) -> Vec<Command> {
+    let mut res: Vec<Command> = Vec::new();
+    for line in input.lines() {
+        let mut nums: Vec<usize> = Vec::new();
+        for c in line.split(" ") {
+            if c.chars().all(|a| a.is_ascii_digit()) {
+                nums.push(c.parse::<usize>().unwrap());
+            }
+        }
+        let (a, b, c) = match &nums[..] {
+            [a, b, c] => (a, b, c),
+            _ => panic!("Invalid input line, too many numbers"),
+        };
+        res.push(Command(*a, *b, *c));
     }
     res
 }
